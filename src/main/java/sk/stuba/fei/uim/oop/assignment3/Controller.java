03ef52522c3b79bb80888d4ae14e2c641024db5d
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2021-05-09 15:15:34 +0200

    Hello controller

---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/Controller.java
@@ -0,0 +1,18 @@
+package sk.stuba.fei.uim.oop.assignment3;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+
+public class Controller {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
