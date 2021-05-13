396a6b7b0c101ec322ee8c6008b82dacf5584921
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2021-05-13 10:25:31 +0200

    everything working

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/cart/CartController.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/cart/CartController.java
@@ -2,7 +2,6 @@ package sk.stuba.fei.uim.oop.assignment3.cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
